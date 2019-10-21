585bd370d1fd36103ebf52f842daed743e5f0859
vadimyanulevich <vadimyanulevich6@gmail.com>
2019-10-21 07:09:12 +0300

    asd

---
+++ Dzielnik/src/Dzielnik.java
@@ -0,0 +1,39 @@
+
+public class Dzielnik
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < args.length; i++)
+        {
+
+            int n=0;
+            try
+            {
+                n = Integer.parseInt(args[i]);
+            }
+            catch (NumberFormatException ex)
+            {
+                System.out.println(args[i] + "Nie jest liczba calkowita");
+            }
+            int b = div(n);
+            System.out.println(args[i] + " " + b);
+        }
+
+
+    }
+        public static int div ( int a)
+        {
+            if (a == 1)
+            {
+                return a;
+            }
+            else
+                {
+                int i = 2;
+                while (a % i != 0) {
+                    i++;
+                }
+                return a / i;
+                }
+        }
+}
